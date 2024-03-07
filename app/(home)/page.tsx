ab4365717dd7818d531b6bb75c686f9de1aa03ba
mfjack <ferreira.marlon@live.com>
2024-03-07 17:27:12 -0300

    feat: add welcome message

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,9 +1,15 @@
+import { ptBR } from "date-fns/locale";
 import { Header } from "../components/header";
+import { format } from "date-fns";
 
 const Home = () => {
   return (
     <>
       <Header />
+      <div className="px-5 pt-5">
+        <h2 className="text-xl font-bold">Olá Jack!</h2>
+        <p className="capitalize text-sm">{format(new Date(), "EEEE',' dd ' de 'MMMM", { locale: ptBR })}</p>
+      </div>
     </>
   );
 }
