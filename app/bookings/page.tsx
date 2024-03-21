cf7c90b518f908b0c931116d5c8bdb8f86eefd39
mfjack <ferreira.marlon@live.com>
2024-03-21 10:58:36 -0300

    refactor: code refactoring, deleting the bookingList file and improving logic in bookingPage

--- app/bookings/page.tsx
+++ app/bookings/page.tsx
@@ -50,7 +50,7 @@ const BookingsPage = async () => {
 			<div className='px-5 py-6'>
 				<h1 className='text-xl font-bold mb-3'>Agendamentos</h1>
 
-				{confirmedBookings.length > 0 && finishedBookings.length === 0 && (
+				{confirmedBookings.length > 0 && (
 					<h2 className='text-gray-400 uppercase font-bold text-sm mb-3'>Confirmados</h2>
 				)}
 				<div className='flex flex-col gap-3'>
@@ -58,15 +58,13 @@ const BookingsPage = async () => {
 						<BookingItem
 							key={booking.id}
 							booking={booking}
-						/>
+						/>	
 					))}
 				</div>
 
-				{finishedBookings.length > 0 && confirmedBookings.length === 0 && (
+				{finishedBookings.length > 0 && (
 					<>
-						<h2 className='text-gray-400 uppercase font-bold text-sm mt-6 mb-3'>
-							Finalizados
-						</h2>
+						<h2 className='text-gray-400 uppercase font-bold text-sm mt-6 mb-3'>Finalizados</h2>
 
 						<div className='flex flex-col gap-3'>
 							{finishedBookings.map(booking => (
