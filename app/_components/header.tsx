ff2bcc49a216078ce2f8dab0b35a58c958383a4c
mfjack <ferreira.marlon@live.com>
2024-03-21 11:11:50 -0300

    feat: ensure semantic tag are used on header

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -10,31 +10,33 @@ import Link from 'next/link';
 
 export const Header = () => {
 	return (
-		<Card>
-			<CardContent className='p-5 flex justify-between flex-row items-center'>
-				<Link href='/'>
-					<Image
-						src='/logo.svg'
-						alt='Logo'
-						width={120}
-						height={22}
-					/>
-				</Link>
-				<Sheet>
-					<SheetTrigger asChild>
-						<Button
-							variant='outline'
-							size={'icon'}
-						>
-							<MenuIcon size={16} />
-						</Button>
-					</SheetTrigger>
+		<header>
+			<Card>
+				<CardContent className='p-5 flex justify-between flex-row items-center'>
+					<Link href='/'>
+						<Image
+							src='/logo.svg'
+							alt='Logo'
+							width={120}
+							height={22}
+						/>
+					</Link>
+					<Sheet>
+						<SheetTrigger asChild>
+							<Button
+								variant='outline'
+								size={'icon'}
+							>
+								<MenuIcon size={16} />
+							</Button>
+						</SheetTrigger>
 
-					<SheetContent className='p-0'>
-						<SideMenu />
-					</SheetContent>
-				</Sheet>
-			</CardContent>
-		</Card>
+						<SheetContent className='p-0'>
+							<SideMenu />
+						</SheetContent>
+					</Sheet>
+				</CardContent>
+			</Card>
+		</header>
 	);
 };
